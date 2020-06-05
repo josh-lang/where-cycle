5840e10e624c3279a9bff95c36a632b8a18d17e3
josh-lang <joshuaclang1@gmail.com>
2020-06-05 03:33:57 +0000

    initial taxi zone conversion, lookup file, and centroid file

---
+++ nyc-tlc/taxi_zone_lookup.py
@@ -0,0 +1,42 @@
+import boto3
+import geopandas as gpd
+import os
+
+
+s3 = boto3.resource('s3')
+s3.meta.client.download_file(
+    'nyc-tlc',
+    'misc/taxi_zones.zip',
+    './tmp/taxi_zones.zip'
+)
+
+original = gpd.read_file('zip://./tmp/taxi_zones.zip')
+converted = original.to_crs('EPSG:4326')
+lookup = converted.filter(
+    [
+        'LocationID',
+        'zone',
+        'borough',
+        'geometry'
+    ]
+    , axis=1
+)
+
+centroid = lookup.filter(['LocationID', 'geometry'], axis=1)
+centroid['geometry'] = centroid['geometry'].centroid
+
+lookup.to_file('../results/taxi_zone_lookup.shp', 'ESRI Shapefile')
+s3.meta.client.upload_file(
+    '../results/taxi_zone_lookup.shp',
+    'jlang-20b-de-ny',
+    'mvp_results/taxi_zone_lookup.shp'
+)
+
+centroid.to_file('../results/taxi_zone_centroids.json', 'GeoJSON')
+s3.meta.client.upload_file(
+    '../results/taxi_zone_centroids.json',
+    'jlang-20b-de-ny',
+    'mvp_results/taxi_zone_centroids.json'
+)
+
+os.remove('./tmp/taxi_zones.zip')
