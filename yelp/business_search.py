fb25fe6696b5c1790ec49b8e3f3e4cad70e9f07a
josh-lang <joshuaclang1@gmail.com>
2020-06-05 13:35:03 +0000

    yelp business ingestion script

--- yelp/business_search.py
+++ yelp/business_search.py
@@ -1,9 +1,35 @@
+import boto3
 import os
 import pandas as pd
 import requests
 
 
-api_key = os.environ['YELP_API_KEY']
-base_url = 'https://api.yelp.com/v3/businesses/search'
-client_id = os.environ['YELP_CLIENT_ID']
+api_key = 'Bearer ' + os.environ['YELP_API_KEY']
+head = {'Authorization': api_key}
 centroids = pd.read_json('../results/taxi_zone_centroids.json', orient='records')
+url = 'https://api.yelp.com/v3/businesses/search'
+
+businesses = pd.DataFrame()
+for index, row in centroids.iterrows():
+    query = {
+        'latitude': row['latitude'],
+        'longitude': row['longitude'],
+        'radius': 3000,
+        'limit': 50,
+        'sort_by': 'distance'
+    }
+
+    res = requests.get(url, headers=head, params=query)
+    matches = res.json()['businesses']
+    for match in matches:
+        match['LocationID'] = int(row['LocationID'])
+    
+    businesses = businesses.append(matches, ignore_index=True)
+
+s3 = boto3.resource('s3')
+businesses.to_json('../results/yelp_businesses.json', orient='records')
+s3.meta.client.upload_file(
+    '../results/yelp_businesses.json',
+    'jlang-20b-de-ny',
+    'mvp_results/yelp_businesses.json'
+)
