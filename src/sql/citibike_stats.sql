a9344717271edb1c13700816ae6d4a094f51be9f
Ubuntu <ubuntu@ip-10-0-0-13.ec2.internal>
2020-06-16 20:13:35 +0000

    citibike and yelp stats in psql

--- src/sql/citibike_stats.sql
+++ src/sql/citibike_stats.sql
@@ -1,14 +1,24 @@
 DROP TABLE IF EXISTS citibike_stats;
 
 CREATE TABLE citibike_stats AS
-	SELECT
-        s.zone_id,
-        v.month,
-        COUNT(s.station_id) AS stations,
-        SUM(v.endpoint_visits) AS endpoint_visits
-    FROM
-        citibike_stations_by_zone AS s
-        JOIN citibike_endpoint_visits AS v
-            USING (station_id)
-    GROUP BY 1, 2
+    SELECT
+        t.month,
+        t.zone_id,
+        MAX(active_stations) OVER (
+            PARTITION BY t.zone_id
+            ORDER BY t.month ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+        ) AS stations,
+        endpoint_visits
+    FROM (
+        SELECT
+            v.month,
+            s.zone_id,
+            COUNT(s.station_id) AS active_stations,
+            SUM(v.endpoint_visits) AS endpoint_visits
+        FROM
+            citibike_stations_by_zone AS s
+            JOIN citibike_endpoint_visits AS v
+                USING (station_id)
+        GROUP BY 1, 2
+    ) AS t
     ORDER BY 1, 2;
